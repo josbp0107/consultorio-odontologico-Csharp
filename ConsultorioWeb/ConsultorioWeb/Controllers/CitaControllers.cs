52757c6513a379f67a446f8e415a56f49b97b9e9
jose B Paternina <josebp0107@gmil.com>
2019-09-17 12:04:12 -0500

    Se agrego la clase CitaController.cs

---
+++ ConsultorioWeb/ConsultorioWeb/Controllers/CitaControllers.cs
@@ -0,0 +1,15 @@
+﻿using Aplicacion.Implementacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsultorioWeb.Controllers
+{
+    public class CitaControllers : Controller
+    {
+        private readonly CitaAplicacion cita = new CitaAplicacion();
+
+    }
+}
